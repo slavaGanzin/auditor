f975c6341046df5375a37fa49f2afc27165f0d8f
Slava Ganzin <slava.ganzin@gmail.com>
2018-07-10 18:51:11 +0300

    sockets ml-39

--- server.js
+++ server.js
@@ -28,25 +28,52 @@ const processFile = (file, cb) => {
     if (e) return console.error(e)
     exec(`sox ${file} ${mp3}; rm ${file}`, () =>
       fs.readFile(txt, 'utf8', (e, transcription) => {
-        outCsv.write(`${transcription},\t${mp3}\n`)
+        outCsv.write(`${transcription},\t${mp3.replace('data/', '')}\n`)
         cb()
       })
     )
   })
 }
 const mp3queue = async.queue(processFile, 10)
-
-mp3queue.drain = function() {
-  readFiles()
-}
-
+mp3queue.drain = readFiles
 map(mp3queue.push, reject(test(/mp3$|txt$/), files))
 
 const app = express()
 app.use(morgan('combined'))
+app.use('/data', serveStatic('data', {cacheControl: false}))
 app.use(serveStatic('static', {cacheControl: false}))
 app.get('/files', (req, res, next) => {
   res.json(transcription2mp3)
 })
 
 app.listen(3000)
+
+const wss = new uws.Server({port: 3001})
+
+wss
+  .on('connection', ws => {
+    ws.on('message', message => {
+      try {
+        const data = JSON.parse(message)
+        const [event] = Object.keys(data)
+
+        for (let f of ws.events[event] || [pp])
+          f(data[event])
+      } catch (e) {
+        pp(e, message)
+      }
+    })
+
+    ws.events = []
+    ws.sentDocs = []
+
+    const emit = curry((event, data) => {
+      if (ws.readyState == ws.OPEN)
+        return ws.send(JSON.stringify({[event]: data}))
+      // ws.buffer.push([event, data])
+      return data
+    })
+
+    const on = (message, f) =>
+      ws.events[message] = concat(defaultTo([pp], ws.events[message]), [f])
+  })
