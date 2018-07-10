443cb3fe5fc26d369f1fc0f8e4f500751679827f
Slava Ganzin <slava.ganzin@gmail.com>
2018-07-10 18:18:23 +0300

    wav -> mp3

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 #!/usr/bin/env node-dev
 
 const fs = require('fs')
-const {spawn} = require('child_process')
+const {exec} = require('child_process')
 
-spawn('pug', '--watch static --name-after-file --pretty --no-debug'.split(' '))
+exec('pug --watch static --name-after-file --pretty --no-debug')
 
 require('auto-require')({
   globaly: true,
   toRoot: ['ramda']
 })
+const NEW = 'data/new/'
 
-app = express()
+const files = map(x => `${NEW}${x}`, fsReaddirRecursive(NEW))
 
+const outCsv = fs.createWriteStream('data/files.csv', {'flags': 'a'})
+const processFile = (file, cb) => {
+  const mp3 = file.replace(/[^.]+$/, 'mp3')
+  const txt = file.replace(/[^.]+$/, 'txt')
+  fs.access(txt, fs.constants.F_OK, e => {
+    if (e) return console.error(e)
+    exec(`sox ${file} ${mp3}; rm ${file}`, () =>
+      fs.readFile(txt, 'utf8', (e, transcription) => {
+        outCsv.write(`"${transcription}",${mp3}\n`)
+        cb()
+      })
+    )
+  })
+}
+const mp3queue = async.queue(processFile, 10)
+
+mp3queue.drain = function() {
+    console.log("All files are uploaded");
+};
+
+map(mp3queue.push, reject(test(/mp3$|txt$/), files))
+
+const app = express()
 app.use(morgan('combined'))
 app.use(serveStatic('static', {cacheControl: false}))
-
 app.listen(3000)
