af2a1dd40649b4fe02a92efedc6d8e695d9d13f3
Slava Ganzin <slava.ganzin@gmail.com>
2018-07-10 18:36:07 +0300

    out csv ml-39

--- server.js
+++ server.js
@@ -10,10 +10,17 @@ require('auto-require')({
   toRoot: ['ramda']
 })
 const NEW = 'data/new/'
+const FILES = 'data/files.csv'
+
+let transcription2mp3 = []
+const readFiles = () => {
+  transcription2mp3 = map(split(',\t'), split('\n', fs.readFileSync(FILES, 'utf8')))
+}
+readFiles()
 
 const files = map(x => `${NEW}${x}`, fsReaddirRecursive(NEW))
 
-const outCsv = fs.createWriteStream('data/files.csv', {'flags': 'a'})
+const outCsv = fs.createWriteStream(FILES, {'flags': 'a'})
 const processFile = (file, cb) => {
   const mp3 = file.replace(/[^.]+$/, 'mp3')
   const txt = file.replace(/[^.]+$/, 'txt')
@@ -21,7 +28,7 @@ const processFile = (file, cb) => {
     if (e) return console.error(e)
     exec(`sox ${file} ${mp3}; rm ${file}`, () =>
       fs.readFile(txt, 'utf8', (e, transcription) => {
-        outCsv.write(`"${transcription}",${mp3}\n`)
+        outCsv.write(`${transcription},\t${mp3}\n`)
         cb()
       })
     )
@@ -30,12 +37,16 @@ const processFile = (file, cb) => {
 const mp3queue = async.queue(processFile, 10)
 
 mp3queue.drain = function() {
-    console.log("All files are uploaded");
-};
+  readFiles()
+}
 
 map(mp3queue.push, reject(test(/mp3$|txt$/), files))
 
 const app = express()
 app.use(morgan('combined'))
 app.use(serveStatic('static', {cacheControl: false}))
+app.get('/files', (req, res, next) => {
+  res.json(transcription2mp3)
+})
+
 app.listen(3000)
