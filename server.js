1ea32712c608841b05b97592cc5ecdaf8d1a3897
Slava Ganzin <slava.ganzin@gmail.com>
2018-07-11 16:07:35 +0300

    it's alive ml-39

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 #!/usr/bin/env node-dev
 
 const fs = require('fs')
-const {spawn} = require('child_process')
-
+const {exec, execSync} = require('child_process')
 
 require('auto-require')({
   globaly: true,
   toRoot: ['ramda']
 })
+
+function pp (...args) {
+  for (let x of args)
+    process.stdout.write(JSON.stringify(x, null, ' ') + '\n')
+  return args
+}
+
 const NEW = 'data/new/'
-const FILES = 'data/files.csv'
+const NEW_CSV = 'data/new.csv'
+const VALIDATED_CSV = 'data/validated.csv'
 
 let files = []
 const readFiles = () => {
-  files = map(split(',\t'), split('\n', fs.readFileSync(FILES, 'utf8')))
+  files = map(split(',\t'), split('\n', fs.readFileSync(NEW_CSV, 'utf8')))
 }
 readFiles()
 
 const newFiles = map(x => `${NEW}${x}`, fsReaddirRecursive(NEW))
+const newCsv = fs.createWriteStream(NEW_CSV, {'flags': 'a'})
+const validatedCsv = fs.createWriteStream(VALIDATED_CSV, {'flags': 'a'})
 
-const outCsv = fs.createWriteStream(FILES, {'flags': 'a'})
 const processFile = (file, cb) => {
   const mp3 = file.replace(/[^.]+$/, 'mp3')
   const txt = file.replace(/[^.]+$/, 'txt')
@@ -27,7 +35,7 @@ const processFile = (file, cb) => {
     if (e) return console.error(e)
     exec(`sox ${file} ${mp3}; rm ${file}`, () =>
       fs.readFile(txt, 'utf8', (e, transcription) => {
-        outCsv.write(`${transcription},\t${mp3.replace('data/', '')}\n`)
+        newCsv.write(`${transcription},\t${mp3.replace('data/', '')}\n`)
         cb()
       })
     )
@@ -41,10 +49,6 @@ const app = express()
 app.use(morgan('combined'))
 app.use('/data', serveStatic('data', {cacheControl: false}))
 app.use(serveStatic('static', {cacheControl: false}))
-app.get('/files', (req, res, next) => {
-  res.json(files)
-})
-
 app.listen(3000)
 
 const wss = new uws.Server({port: 3001})
@@ -77,4 +81,11 @@ wss
       ws.events[message] = concat(defaultTo([pp], ws.events[message]), [f])
 
     emit('files', files)
+    on('grade', ({text, quality, audio}) => {
+      const validatedAudio = audio.replace('new', 'validated')
+
+      execSync(`sh -c "mkdir -p \`dirname ${validatedAudio}\` && mv ${audio} ${validatedAudio}"`)
+
+      validatedCsv.write(`"${text.replace('"', "'")}",${validatedAudio},${quality}\n`)
+    })
   })
--- static/app.js
+++ static/app.js
@@ -16,21 +16,31 @@ map(template => {
 let fileIndex = 200
 
 function blurAll(){
- var tmp = document.createElement("input");
- document.body.appendChild(tmp);
- tmp.focus();
- document.body.removeChild(tmp);
+  const tmp = document.createElement("input")
+  document.body.appendChild(tmp)
+  tmp.focus()
+  document.body.removeChild(tmp)
 }
 const redrawFiles = () => {
   I('card').innerHTML = T.card(FILES[fileIndex])
   Mousetrap(I('text')).bind('escape', e => {
     blurAll()
   })
-
 }
 
-const rate = n =>
-  alert(n)
+const nextFile = (k = 1) => {
+  fileIndex = clamp(0, length(FILES), fileIndex + k)
+  console.log(fileIndex)
+  redrawFiles()
+}
+
+const grade = quality => {
+  const text = I("text").innerText
+  const audio = FILES[fileIndex][1]
+  ws.emit('grade', {text, quality, audio})
+  nextFile()
+}
+
 ws.on('files', compose(redrawFiles, files => window.FILES = files))
 
 Mousetrap
@@ -41,10 +51,10 @@ Mousetrap
   .bind('space', () => I('audio').paused ? I('audio').play() : I('audio').pause())
   .bind('left',  () => I('audio').currentTime = clamp(0, I('audio').duration,  I('audio').currentTime - 5))
   .bind('right', () => I('audio').currentTime = clamp(0, I('audio').duration-1,  I('audio').currentTime + 5))
-  .bind('up', compose(redrawFiles, () => fileIndex = clamp(0, length(FILES), fileIndex - 1)))
-  .bind('down', compose(redrawFiles, () => ++fileIndex))
-  .bind('1', () => rate(1))
-  .bind('2', () => rate(2))
-  .bind('3', () => rate(3))
-  .bind('4', () => rate(4))
-  .bind('5', () => rate(5))
+  .bind('up', () => nextFile(-1))
+  .bind('down', () => nextFile(1))
+  .bind('1', () => grade(1))
+  .bind('2', () => grade(2))
+  .bind('3', () => grade(3))
+  .bind('4', () => grade(4))
+  .bind('5', () => grade(5))
