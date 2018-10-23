b03fadbee519f14d662907b69c50661fb53dba8c
Slava Ganzin <slava.ganzin@gmail.com>
2018-10-23 19:09:41 +0300

    multi-file duration ML-43

--- duration.js
+++ duration.js
@@ -1,34 +1,54 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const cp = require('child_process')
+const path = require('path')
+const fsReaddirRecursive = require('fs-readdir-recursive')
 
 for (let k in require('ramda'))
   global[k] = require('ramda')[k]
 
-const path =  require('path')
-
-const csv = fs.readFileSync(process.argv[2], 'utf8')
-const d = path.resolve(path.dirname(process.argv[2]))
-
-const getDuration = compose(x=>parseFloat(pathOr("0", ['streams_stream_0_duration'], x).replace(/"/g,'')), fromPairs, map(split('=')), split('\n'))
-
 let errors =0
 let total = 0
 
-pipe(
+const parseDuration = compose(
+  parseFloat,
+  replace(/"/g,''),
+  pathOr("0", ['streams_stream_0_duration']),
+  fromPairs,
+  map(split('=')),
+  split('\n')
+)
+
+const getDuration = x => new Promise((resolve, reject) =>
+  cp.exec(`ffprobe -v error -of flat=s=_ -select_streams a:0 -show_streams "${x}"`, {encoding: 'utf8'}, (e, stdout, stderr) => {
+    total++
+    // console.log(e)
+    if (!e) return resolve(parseDuration(stdout))
+    errors++
+    resolve(0)
+  }))
+
+const getDurationsOfOneFile = ([d, csv]) => pipe(
   split('\n'),
+  map(replace(/,[^,]+validated([^,]+)/, `,/$1`)),
   map(replace(/.*,([^,]*mp3).*/g, `${d}/$1`)),
-  map(x => new Promise((resolve, reject) =>
-    cp.exec(`ffprobe -v error -of flat=s=_ -select_streams a:0 -show_streams "${x}"`, {encoding: 'utf8'}, (e, stdout, stderr) => {
-      total++
-      console.log(e)
-      if (!e) return resolve(getDuration(stdout))
-      errors++
-      resolve(0)
-    }))),
-  x => Promise.all(x),
+  map(replace(/\r/g, ``)),
+  map(replace(/\/\//g, `/`)),
+  map(getDuration),
 )(csv)
-  .then(reject(gt(60)))
+
+const validated = pipe(
+  fsReaddirRecursive,
+  filter(test(/\.csv$/)),
+  map(x => [
+    path.dirname(`${process.argv[2]}/${x}`),
+    fs.readFileSync(`${process.argv[2]}/${x}`, 'utf8')
+  ])
+)
+
+Promise.all(flatten(map(getDurationsOfOneFile, validated(process.argv[2]))))
+  .then(flatten)
+  .then(reject(x => x > 60))
   .then(sum)
   .then(divide(__, 60*60))
-  .then(x => console.log(x, "\nerrors:\t", errors, "\ntotal:\t", total))
+  .then(x => console.log(x + "\nerrors:\t", errors, "\ntotal:\t", total))
